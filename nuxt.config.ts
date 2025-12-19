c826b40f1e29dae421218121002f37750f3dc2e1
夏沫花火zzz🌙 <xmcn@1l1.icu>
2025-12-19 18:16:21 +0800

    Add Umami analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,17 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint'],
+  modules: ['@nuxt/eslint', 'nuxt-umami'],
   routeRules: {
     '/': { redirect: '/getting-started' },
   },
   site: {
     name: "LoCyanFrp 帮助文档",
+  },
+  umami: {
+    enabled: true,
+    id: "321f68d2-1667-450a-ac30-22166d693b6d",
+    host: "https://analytics.locyan.cn",
+    autoTrack: true,
+    ignoreLocalhost: true,
   },
 })
